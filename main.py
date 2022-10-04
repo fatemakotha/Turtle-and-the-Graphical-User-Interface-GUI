95625f98e88f502857dcf66c945a1f24c424491e
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-04 22:02:58 +0600

    Refactored/renamed cutu_the_turtle to cutu

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from turtle import Turtle, Screen
 
 #Create a new Turtle OBJECT named cutu_the_turtle
-cutu_the_turtle = Turtle()
+cutu = Turtle()
 # print(cutu_the_turtle) #window opens but dissapears
-cutu_the_turtle.shape("turtle") #makes the arrow change into a turtle
-cutu_the_turtle.color("dark cyan")
+cutu.shape("turtle") #makes the arrow change into a turtle
+cutu.color("dark cyan")
 
 #How to make the object do certain things/FUNCTIONS
 for _ in range(4):
-    cutu_the_turtle.forward(100) #moves forward by a 100 paces
-    cutu_the_turtle.left(90.0) #turns right by 90 degress
+    cutu.forward(100) #moves forward by a 100 paces
+    cutu.left(90.0) #turns right by 90 degress
 
 
 
@@ -22,4 +22,4 @@ for _ in range(4):
 
 
 screen  = Screen()
-screen.exitonclick() #makes the window stay longer
+screen.exitonclick() #makes the window stay longer #THIS NEEDS TO BE AT THE END OF THE CODE *
