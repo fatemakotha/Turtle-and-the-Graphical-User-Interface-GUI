ebefc22071f96264f258f0be10e2fb2636fb14c8
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-04 21:51:35 +0600

    Turtle moves forward by a 100 paces

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ cutu_the_turtle.shape("turtle") #makes the arrow change into a turtle
 cutu_the_turtle.color("dark cyan")
 
 #How to make the object do certain things/FUNCTIONS
+cutu_the_turtle.forward(100) #moves forward by a 100 paces
 
 
 
