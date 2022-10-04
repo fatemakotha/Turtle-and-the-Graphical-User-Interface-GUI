51ebe2e877ea4f934754dd45b6bceef71f662e9b
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-04 21:51:56 +0600

    Turtle turns right by 90 degrees

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cutu_the_turtle.color("dark cyan")
 
 #How to make the object do certain things/FUNCTIONS
 cutu_the_turtle.forward(100) #moves forward by a 100 paces
-
+cutu_the_turtle.right(90.0) #turns right by 90 degress
 
 
 
