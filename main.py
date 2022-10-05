bf3b95e4c3cb9a4a84d74a9d06bd0580df18dc91
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-05 15:53:18 +0600

    Changed speed of turtle

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ import random
 #Create a new Turtle OBJECT named cutu_the_turtle
 cutu = Turtle()
 # print(cutu_the_turtle) #window opens but dissapears
+
+
 cutu.shape("turtle") #makes the arrow change into a turtle
 cutu.color("dark cyan")
 cutu.pensize(10) #increases the width of the pen
+#CHANGING the SPEED of the turtle. 0 OR "fastest", 10 OR "fast", 6 OR "normal", 3 OR "slow", 1 OR "slowest" ***
+cutu.speed(10) #we can either insert the int or the string. i.e. 0 or "fastest"
 
 colours = ["blue", "red", "pink", "black", "purple", "green", "yellow"]
+
 cutu.forward(100)
 
 directions = [0, 90, 180, 270]
 
-for _ in range(20):
+for _ in range(200):
     cutu.pencolor(random.choice(colours))
     cutu.forward(30) #moves 30 units
     cutu.setheading(random.choice(directions)) #changes the left&right to the degrees in the direction list
